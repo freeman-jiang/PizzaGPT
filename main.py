4da211cd43b95aefb0c2cbf3cfc9aeb2890eec49
Shahan Neda <shahan.neda@gmail.com>
2023-10-28 12:44:20 -0400

    added support for examples

--- main.py
+++ main.py
@@ -23,7 +23,17 @@ if len(argv) >= 2:
             + "exercise caution when running suggested commands."
         )
 
-prompt_template = """
+import os
+
+examples = []
+for filename in os.listdir("./examples"):
+    if filename.endswith(".txt"):
+        with open(os.path.join("./examples", filename), "r") as f:
+            examples.append(f.read())
+print(examples)
+
+newline = "\n"
+prompt_template = f"""
 You are an agent controlling a browser. You are given:
 
 	(1) an objective that you are trying to achieve
@@ -49,94 +59,12 @@ Images are rendered as their alt text like this:
 		<img id=4 alt=""/>
 
 Based on your given objective, issue whatever command you believe will get you closest to achieving your goal.
-
 Don't try to interact with elements that you can't see.
 
 Here are some examples:
 
-EXAMPLE 1:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<link id=1>About</link>
-<link id=2>Store</link>
-<link id=3>Gmail</link>
-<link id=4>Images</link>
-<link id=5>(Google apps)</link>
-<link id=6>Sign in</link>
-<img id=7 alt="(Google)"/>
-<input id=8 alt="Search"></input>
-<button id=9>(Search by voice)</button>
-<button id=10>(Google Search)</button>
-<button id=11>(I'm Feeling Lucky)</button>
-<link id=12>Advertising</link>
-<link id=13>Business</link>
-<link id=14>How Search works</link>
-<link id=15>Carbon neutral since 2007</link>
-<link id=16>Privacy</link>
-<link id=17>Terms</link>
-<text id=18>Settings</text>
-------------------
-OBJECTIVE: Find a 2 bedroom house for sale in Anchorage AK for under $750k
-CURRENT URL: https://www.google.com/
-YOUR COMMAND: 
-TYPESUBMIT 8 "anchorage redfin"
-==================================================
+{"---".join(["-----" + newline +  f"EXAMPLE {i}" + newline + ex + newline + "------" + newline for i, ex in enumerate(examples)])}
 
-EXAMPLE 2:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<link id=1>About</link>
-<link id=2>Store</link>
-<link id=3>Gmail</link>
-<link id=4>Images</link>
-<link id=5>(Google apps)</link>
-<link id=6>Sign in</link>
-<img id=7 alt="(Google)"/>
-<input id=8 alt="Search"></input>
-<button id=9>(Search by voice)</button>
-<button id=10>(Google Search)</button>
-<button id=11>(I'm Feeling Lucky)</button>
-<link id=12>Advertising</link>
-<link id=13>Business</link>
-<link id=14>How Search works</link>
-<link id=15>Carbon neutral since 2007</link>
-<link id=16>Privacy</link>
-<link id=17>Terms</link>
-<text id=18>Settings</text>
-------------------
-OBJECTIVE: Make a reservation for 4 at Dorsia at 8pm
-CURRENT URL: https://www.google.com/
-YOUR COMMAND: 
-TYPESUBMIT 8 "dorsia nyc opentable"
-==================================================
-
-EXAMPLE 3:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<button id=1>For Businesses</button>
-<button id=2>Mobile</button>
-<button id=3>Help</button>
-<button id=4 alt="Language Picker">EN</button>
-<link id=5>OpenTable logo</link>
-<button id=6 alt ="search">Search</button>
-<text id=7>Find your table for any occasion</text>
-<button id=8>(Date selector)</button>
-<text id=9>Sep 28, 2022</text>
-<text id=10>7:00 PM</text>
-<text id=11>2 people</text>
-<input id=12 alt="Location, Restaurant, or Cuisine"></input> 
-<button id=13>Let’s go</button>
-<text id=14>It looks like you're in Peninsula. Not correct?</text> 
-<button id=15>Get current location</button>
-<button id=16>Next</button>
-------------------
-OBJECTIVE: Make a reservation for 4 for dinner at Dorsia in New York City at 8pm
-CURRENT URL: https://www.opentable.com/
-YOUR COMMAND: 
-TYPESUBMIT 12 "dorsia new york city"
 ==================================================
 
 The current browser content, objective, and current URL follow. Reply with your next command to the browser.
@@ -151,6 +79,8 @@ CURRENT URL: $url
 PREVIOUS COMMAND: $previous_command
 YOUR COMMAND:
 """
+
+print(prompt_template)
 
 black_listed_elements = set(
     ["html", "head", "title", "meta", "iframe", "body", "script", "style", "path", "svg", "br", "::marker", ])
@@ -566,7 +496,7 @@ if (
                 )
         # response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0.5, best_of=10, n=3,
                                             # max_tokens=50)
-        print(response)
+        # print(response)
         msg = response.choices[0].message.content
 
         return msg
@@ -598,11 +528,16 @@ if (
 
         time.sleep(2)
 
-    objective = "Make a reservation for 2 at 7pm at bistro vida in menlo park"
-    print("\nWelcome to natbot! What is your objective?")
-    i = "Order pizza"
-    if len(i) > 0:
-        objective = i
+    # objective = "Make a reservation for 2 at 7pm at bistro vida in menlo park"
+    # print("\nWelcome to natbot! What is your objective?")
+    # i = "Order pizza"
+    # if len(i) > 0:
+    #     objective = i
+    # print("\nWelcome to natbot! What is your objective?")
+    # i = input()
+    # if len(i) > 0:
+    #     objective = i
+    objective =  "Order a pizza to 151 Charles St W Suite 100, Kitchener, ON N2G 1H6. Pay using cash."
 
     gpt_cmd = ""
     prev_cmd = ""
