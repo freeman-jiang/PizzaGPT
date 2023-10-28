1dba691a3df51629d1f44a0d2b13fbf77c02301f
Shahan Neda <shahan.neda@gmail.com>
2023-10-28 17:49:56 -0400

    added phone

--- twillio/main.py
+++ twillio/main.py
@@ -69,7 +69,7 @@ concat_response = ""
 last_processed: datetime.datetime = datetime.datetime.now()
 messages: list[dict[str, str]] = [
     {"role": "system", "content": "You are talking to a pizza operator over the phone"},
-    {"role": "user", "content": "You are a person at Communitech, which is an office space at 151 Charles Street West, Suite 100. You are on the phone. You will pay by cash later. Answer the pizza operator as succinctly as you can. In your first message, please say explicitly that you want to want to order the pizza to Communitech and state the address. Remember, you are not the pizza operator. You are trying to order pizza. Only order a pizza and no other add ons. Do not give up, order the pizza at all costs, being flexible when needed."},
+    {"role": "user", "content": "You are a person at Communitech, which is an office space at 151 Charles Street West, Suite 100. You are on the phone. You will pay by cash later. Your phone number is 604-441-1171. Answer the pizza operator as succinctly as you can. In your first message, please say explicitly that you want to want to order the pizza to Communitech and state the address. Remember, you are not the pizza operator. You are trying to order pizza. Only order a pizza and no other add ons. Do not give up, order the pizza at all costs, being flexible when needed."},
 ]
 
 
@@ -163,7 +163,8 @@ def make_call():
 
     print(f"Location {location} phone {phone}")
     call = twilio_client.calls.create(
-        to=number_to_call,  # Replace with the desired 'to' number
+        # to=number_to_call,  # Replace with the desired 'to' number
+        to="+" + phone,  # Replace with the desired 'to' number
         from_=twilio_phone_number,  # Your Twilio phone number
         url=public_url + '/call',
     )
--- twillio/start.py
+++ twillio/start.py
@@ -23,7 +23,7 @@ url = "http://127.0.0.1:5000/make_call"
 user_prompt = input("Enter your prompt: ")
 messages: list[dict[str, str]] = [
     {"role": "user", "content": 
-    "Output one word containing LOCATION for the location of the spot where the user wants to order pizza from. The possible LOCATION are: 'PizzaPizza', 'PizzaNova', 'Dominos'."},
+    "Output one word containing LOCATION for the location of the spot where the user wants to order pizza from. The possible LOCATION are: 'PizzaPizza', 'PizzaNova', 'Dominos'. Make sure you keep capitlization and type exactly as before!!"},
     {"role": "user", "content": user_prompt}
 ]
 
@@ -32,6 +32,12 @@ phone_numbers = {
     'Dominos': '+15197452222',
     'PizzaPizza': '+15197471111',
 }
+
+# phone_numbers = {
+#     'PizzaNova': '12365187890',
+#     'Dominos': '12365187890',
+#     'PizzaPizza': '12365187890',
+# }
 response = openai.ChatCompletion.create(
                     model="gpt-3.5-turbo",
                     messages=messages
@@ -40,5 +46,5 @@ response = openai.ChatCompletion.create(
 location = response["choices"][0]["message"]["content"]
 phone = phone_numbers[location]
 print(response)
-print(url + f'?location={location}?phone={phone}')
-requests.get(url + f'?location={location}?phone={phone}')
+print(url + f'?location={location}&phone={phone}')
+requests.get(url + f'?location={location}&phone={phone}')
